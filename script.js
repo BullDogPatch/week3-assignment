3ae17697eaef94d3d5b3e383d3b29eb743314a30
Craig Clayton <craig__clayton@outlook.com>
2024-12-19 15:20:57 +0000

    got my list items made and appended everything, not sure if classNamse are needed as can probably style li itself

--- script.js
+++ script.js
@@ -18,10 +18,30 @@ const getUpgrades = async () => {
 const renderUpgrades = async () => {
   const upgrades = await getUpgrades();
   const ul = document.createElement('ul');
+  ul.className = 'upgrades';
+
   upgrades.forEach((upgrade, idx) => {
-    console.log(idx, upgrade.name);
     const li = document.createElement('li');
-    li.textContent = upgrade.name;
+    li.className = 'upgrade-item';
+    const item = document.createElement('p');
+    item.className = 'item';
+    item.textContent = upgrade.name;
+    li.appendChild(item);
+
+    const price = document.createElement('p');
+    price.className = 'price';
+    price.textContent = upgrade.cost;
+    li.appendChild(price);
+
+    const increasedBy = document.createElement('p');
+    increasedBy.className = 'increase';
+    increasedBy.textContent = upgrade.increase;
+    li.appendChild(increasedBy);
+
+    const buyButton = document.createElement('button');
+    buyButton.className = 'buy';
+    buyButton.textContent = 'Buy';
+    li.appendChild(buyButton);
     console.log(li);
   });
 };
