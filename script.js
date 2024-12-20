d7f26a65e3382f4f16f9c77c8e0fadbd16726ff2
Craig Clayton <craig__clayton@outlook.com>
2024-12-20 12:56:40 +0000

    add this to get cookies per second working but screen is not updating and can't see what I am missing

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gameState = {
-  cookieCount: 2000000,
+  cookieCount: 0,
   cookiesPerSecond: 0,
 };
 
@@ -7,6 +7,7 @@ const upgradesShopContainer = document.querySelector('.upgrades-shop');
 const actualCookieValue = document.querySelector('.actual-cookies');
 const incrementCookies = document.querySelector('.cookie-incrementer');
 const notEnoughCookiesMessage = document.querySelector('.not-enough-cookies');
+const cps = document.querySelector('.cps');
 
 incrementCookies.addEventListener('click', () => {
   gameState.cookieCount++;
@@ -24,39 +25,6 @@ const getUpgrades = async () => {
     console.log("Joe's API is borked");
   }
 };
-
-// const renderUpgrades = async () => {
-//   const upgrades = await getUpgrades();
-//   const ul = document.createElement('ul');
-//   ul.className = 'upgrades';
-
-//   upgrades.forEach((upgrade, idx) => {
-//     const li = document.createElement('li');
-//     li.className = 'upgrade-item';
-//     const item = document.createElement('p');
-//     item.className = 'item';
-//     item.textContent = upgrade.name;
-//     li.appendChild(item);
-
-//     const price = document.createElement('p');
-//     price.className = 'price';
-//     price.textContent = `$C ${upgrade.cost}`;
-//     li.appendChild(price);
-
-//     const increasedBy = document.createElement('p');
-//     increasedBy.className = 'increase';
-//     increasedBy.textContent = `+${upgrade.increase}`;
-//     li.appendChild(increasedBy);
-
-//     const buyButton = document.createElement('button');
-//     buyButton.className = 'buy';
-//     buyButton.textContent = 'Buy';
-//     buyButton.addEventListener('click', () => buyUpgradesHandler(upgrade));
-//     li.appendChild(buyButton);
-//     ul.appendChild(li);
-//   });
-//   upgradesShopContainer.appendChild(ul);
-// };
 
 const renderUpgrades = async () => {
   const upgrades = await getUpgrades();
@@ -98,7 +66,7 @@ const buyUpgradesHandler = (value) => {
   if (gameState.cookieCount >= value.cost) {
     gameState.cookieCount -= value.cost;
     console.log(value.increase);
-    gameState.cookiesPerSecond += Number(value.increase);
+    gameState.co += Number(value.increase);
     actualCookieValue.textContent = gameState.cookieCount;
   } else {
     setTimeout(() => {
@@ -113,6 +81,8 @@ const buyUpgradesHandler = (value) => {
 };
 
 setInterval(() => {
+  gameState.cookieCount++;
   gameState.cookieCount += gameState.cookiesPerSecond;
   actualCookieValue.textContent = gameState.cookieCount;
+  cps.textContent = gameState.cookiesPerSecond;
 }, 1000);
